67e213793bac2e70774a8521e39bcc1322f72955
Oleksandra Serkes <alexsandra.serkes@gmail.com>
2022-09-18 00:02:08 +0300

    Add slider to client block

--- js/slider.js
+++ js/slider.js
@@ -1,2 +1,71 @@
 const slideContainer = document.querySelector(".client__slider");
 const slide = document.querySelector(".client__slides");
+const nextBtn = document.querySelector(".btn__arrow-right");
+const prevBtn = document.querySelector(".btn__arrow-left");
+const interval = 3000;
+
+let slides = document.querySelectorAll(".client__slide-box");
+let index = 1;
+let slideId;
+
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
+
+firstClone.id = "first-clone";
+lastClone.id = "last-clone";
+
+slide.append(firstClone);
+slide.prepend(lastClone);
+
+const slideWidth = slides[index].clientWidth;
+
+slide.style.transform = `translateX(${-slideWidth * index}px)`;
+
+const startSlide = () => {
+  slideId = setInterval(() => {
+    moveToNextSlide();
+  }, interval);
+};
+
+const getSlides = () => document.querySelectorAll(".client__slide-box");
+
+slide.addEventListener("transitionend", () => {
+  slides = getSlides();
+  if (slides[index].id === firstClone.id) {
+    slide.style.transition = "none";
+    index = 1;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+
+  if (slides[index].id === lastClone.id) {
+    slide.style.transition = "none";
+    index = slides.length - 2;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+});
+
+const moveToNextSlide = () => {
+  slides = getSlides();
+  if (index >= slides.length - 1) return;
+  index++;
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  slide.style.transition = "0.7s";
+};
+
+const moveToPreviousSlide = () => {
+  if (index <= 0) return;
+  index--;
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  slide.style.transition = "0.7s";
+};
+
+slideContainer.addEventListener("mouseenter", () => {
+  clearInterval(slideId);
+});
+
+slideContainer.addEventListener("mouseleave", startSlide);
+
+nextBtn.addEventListener("click", moveToNextSlide);
+prevBtn.addEventListener("click", moveToPreviousSlide);
+
+startSlide();
